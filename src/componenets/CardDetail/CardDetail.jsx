383e3e2d6f993e3358fbc2f6ce67936cee9355ac
Zisan05 <mdzisanislam69@gmail.com>
2023-10-07 04:57:26 -0700

    card detial page done

--- src/componenets/CardDetail/CardDetail.jsx
+++ src/componenets/CardDetail/CardDetail.jsx
@@ -1,11 +1,14 @@
 
 
 const CardDetail = ({cardData}) => {
-const {image} = cardData;
+const {image,name,price,description} = cardData;
      
     return (
         <div>
             <img className="w-full h-[500px]" src={image} alt="" />
+            <h1 className="text-[30px] text-blue-300"><span className="font-bold text-orange-400">Event Name: </span>{name}</h1>
+            <h1 className="text-[30px] text-blue-300"><span className="font-bold text-orange-400">Price: </span>{price}</h1>
+            <h1 className="text-[30px] text-blue-300"><span className="font-bold text-orange-400">Description: </span>{description}</h1>
         </div>
     );
 };
